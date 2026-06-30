84dd783b562a4416c0ae4a3651fbd03c45407cfa
Jyung01 <wldnd9895@naver.com>
2026-06-30 11:40:17 +0900

    commit 0.0.2

--- src/main/java/kr/co/sboard/repository/UserRepository.java
+++ src/main/java/kr/co/sboard/repository/UserRepository.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    int countByUserid(String userid);
+    int countByNick(String nick);
+    int countByEmail(String email);
+    int countByHp(String hp);
 }
