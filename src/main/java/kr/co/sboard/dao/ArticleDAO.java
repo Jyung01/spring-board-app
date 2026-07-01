e96c3ed2575426972778e703343c8b7407533256
Jyung01 <wldnd9895@naver.com>
2026-07-01 10:46:07 +0900

    commit 0.0.4

--- src/main/java/kr/co/sboard/dao/ArticleDAO.java
+++ src/main/java/kr/co/sboard/dao/ArticleDAO.java
@@ -10,7 +10,8 @@ public interface ArticleDAO {
 
     public void insert(ArticleDTO dto);
     public ArticleDTO select(int ano);
-    public List<ArticleDTO> selectAll();
+    public List<ArticleDTO> selectAll(int start);
+    public int selectCountAll();
     public void update(ArticleDTO dto);
     public void delete(int ano);
 }
